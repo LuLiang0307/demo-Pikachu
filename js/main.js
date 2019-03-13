582069ed484c504878663b0090d160a66bd56bfb
lianglu <18873220186@163.com>
2019-03-13 22:52:03 +0800

    copy jquery

--- js/main.js
+++ js/main.js
@@ -1,19 +1,5 @@
+let duration = 50
 !function(){
-    function writeCode(prefix, code, fn){
-        let container = document.querySelector('#code')
-        let styleTag = document.querySelector('#styleTag')
-        let n = 0
-        let id = setInterval(()=>{
-            n+=1
-            container.innerHTML = code.substring(0, n)
-            styleTag.innerHTML = code.substring(0, n)
-            container.scrollTop = container.scrollHeight
-            if(n >= code.length){
-                window.clearInterval(id)
-                fn && fn.call()
-            }
-        },10)
-    }
     let code = `
 /*
 *首先，需要皮卡丘的皮
@@ -166,4 +152,39 @@
 */
 `
     writeCode('', code)
+
+    $('.actions').on('click','button',function(e){
+        let $button = $(e.currentTarget)
+        let speed = $button.attr('data-speed')
+        console.log(speed)
+        $button.addClass('active')
+        .siblings('.active').removeClass('active')
+        switch(speed){
+            case 'slow': 
+                duration = 100
+                break
+            case 'normal': 
+                duration = 50
+                break
+            case 'fast':
+                duration = 10
+                break
+        }
+    })
+    function writeCode(prefix, code, fn){
+        let container = document.querySelector('#code')
+        let styleTag = document.querySelector('#styleTag')
+        let n = 0
+        let id = setTimeout(function run(){
+            n+=1
+            container.innerHTML = code.substring(0, n)
+            styleTag.innerHTML = code.substring(0, n)
+            container.scrollTop = container.scrollHeight
+            if(n < code.length){
+                id = setTimeout(run,duration)
+            }else{
+                fn && fn.call()
+            }
+        },duration)
+    }
 }.call()
